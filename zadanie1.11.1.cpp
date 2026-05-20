8c8ac4839700daee7c4a0aa72c430e6bc1491f51
Konststep <Rain1-1@mail.ru>
2026-05-20 11:06:21 +0300

    Add files via upload

---
+++ zadanie1.11.1.cpp
@@ -0,0 +1,21 @@
+﻿//#include <iostream>
+#include <clocale>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string str1, str2;
+
+    cout << "Введите имя: ";
+    cin >> str1;
+    cout << "Введите фамилию: ";
+    cin >> str2;
+    cout << "Здравствуйте, " + str1 + " " + str2 << endl;
+
+    return EXIT_SUCCESS;
+}
