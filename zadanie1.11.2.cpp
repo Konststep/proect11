01053a8ab55547a52ad6717c8b5149b43f41814f
Konststep <Rain1-1@mail.ru>
2026-05-20 11:33:41 +0300

    Add files via upload

---
+++ zadanie1.11.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string str1, str2;
+    str1 = {"малина"};
+    
+    cout << "Угадайте слово: ";
+    cin >> str2;
+
+    while (str1.compare(str2)) {
+        cout << "Неправильно" << endl;
+        cout << "Угадайте слово: ";
+        cin >> str2;
+    }
+    cout << "Правильно! Вы победили! Загаданное слово — " + str1 << endl;
+    
+
+    return EXIT_SUCCESS;
+}
